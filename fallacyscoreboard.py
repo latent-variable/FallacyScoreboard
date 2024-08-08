29e796738623c921b855c167c5bf81d105ee9f56
latent-variable <lino.valdovinos.cs@gmail.com>
2024-08-07 23:37:46 -0700

    Model can now add gifs

--- fallacyscoreboard.py
+++ fallacyscoreboard.py
@@ -4,10 +4,12 @@ import time
 import ollama
 import yt_dlp
 import random
+import requests
 import colorsys
 import whisperx
 
 from pathlib import Path
+from contextlib import contextmanager
 from moviepy.config import change_settings
 from moviepy.editor import VideoFileClip, TextClip,  ColorClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
@@ -21,8 +23,16 @@ YOUR_HF_TOKEN = None
 
 # OLLAMA host & model
 OLLAMA_HOST  = 'http://192.168.50.81:11434' #'http://localhost:11434'
-OLLAMA_MODEL =  'lucas2024/gemma-2-9b-it-sppo-iter3:q8_0' #'llama3.1' #'gemma2:27b' #'llama3.1:70b' 
-OLLAMA_CONTEXT_SIZE = 4_096 # Max context size for OLLAMA is 
+OLLAMA_MODEL =  'gemma2:27b' #'lucas2024/gemma-2-9b-it-sppo-iter3:q8_0' #'llama3.1' #'llama3.1:70b' 
+OLLAMA_CONTEXT_SIZE = 8_000 # Max context size for OLLAMA is 
+
+
+def read_config(config_file="config.json"):
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+    return config["GIPHY_API_KEY"], config["YOUR_HF_TOKEN"]
+
+GIPHY_API_KEY, YOUR_HF_TOKEN = read_config()
 
 def download_youtube_video(url, video_name):
     ydl_opts = {
@@ -223,7 +233,7 @@ def extract_text_segment(line):
 def validate_llm_output(output, input_text_segment):
     try:
         data = output
-        required_fields = ["text_segment", "fallacy_explanation", "fallacy_type", "speaker", "start", "end"]
+        required_fields = ["text_segment", "fallacy_explanation", "fallacy_type", "speaker", "start", "end", "gif_query"]
         
         # Check if all required fields are present
         for field in required_fields:
@@ -245,6 +255,8 @@ def validate_llm_output(output, input_text_segment):
             return False, "start should be a number"
         if not isinstance(data["end"], (int, float)):
             return False, "end should be a number"
+        if not isinstance(data["gif_query"], str):
+            return False, "giphy_search_query should be a string"
 
         # Check if text_segment matches the input text segment content
         if data["text_segment"].strip() != input_text_segment.strip():
@@ -253,6 +265,7 @@ def validate_llm_output(output, input_text_segment):
         return True, ""
     except (json.JSONDecodeError, TypeError):
         return False, "Invalid JSON format"
+
 
 def read_line_from_file(file_path):
     with open(file_path, 'r') as file:
@@ -272,12 +285,72 @@ def save_intermediate_results(temp_file, history, llm_outputs, processed_lines):
     with open(temp_file, 'w') as f:
         json.dump(temp_data, f, indent=2)
 
-def generate_pastel_color():
-    h = random.random()
-    s = 0.5 + random.random() * 0.5  # 0.5 to 1.0
-    l = 0.7 + random.random() * 0.2  # 0.7 to 0.9
-    r, g, b = [int(256*i) for i in colorsys.hls_to_rgb(h, l, s)]
-    return f'#{r:02x}{g:02x}{b:02x}'
+
+def search_and_download_gif(query, output_path, limit = 5):
+    if query is None or output_path is None or query == "" or output_path == "":
+        return None
+    
+    url = f"https://api.giphy.com/v1/gifs/search"
+    params = {
+        "api_key": GIPHY_API_KEY,
+        "q": query,
+        "limit": limit  # Get limit number of options to choose from
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    
+    if data["data"]:
+        selected_gif = random.choice(data["data"])
+        gif_url = selected_gif["images"]["original"]["url"]
+        gif_response = requests.get(gif_url)
+        
+        with open(output_path, 'wb') as f:
+            f.write(gif_response.content)
+        
+        return output_path
+    else:
+        print(f"No GIF found for the query: {query}")
+        return None
+
+def generate_pastel_colors(num_colors):
+    colors = []
+    for i in range(num_colors):
+        hue = i / num_colors
+        saturation = 0.4 + random.random() * 0.2  # Keep saturation low for pastel colors
+        lightness = 0.6 + random.random() * 0.2  # Keep lightness high for pastel colors
+        r, g, b = [int(256 * channel) for channel in colorsys.hls_to_rgb(hue, lightness, saturation)]
+        colors.append(f'#{r:02x}{g:02x}{b:02x}')
+    random.shuffle(colors)  # Shuffle to randomize the order
+    return colors
+
+@contextmanager
+def close_clip(clip):
+    try:
+        yield clip
+    finally:
+        if hasattr(clip, 'reader'):
+            clip.reader.close()
+        if hasattr(clip, 'audio') and hasattr(clip.audio, 'reader'):
+            clip.audio.reader.close_proc()
+        clip.close()
+        
+def add_gif_to_video(original_height, new_height, new_width, gif_path, start_time, end_time, sidebar_width):
+
+    gif = VideoFileClip(gif_path)
+    # Resize GIF to fit within the video frame if necessary
+    gif = gif.resize(height=original_height // 4.5)
+    
+    # Calculate position for the center of the black vertical panel on the right side
+    position = (new_width - sidebar_width + (sidebar_width - gif.size[0]) // 2, (new_height - gif.size[1]) )
+    
+    # Calculate the duration for the GIF to play
+    duration = end_time - start_time
+    
+    # Loop the GIF to match the desired duration
+    gif = gif.loop(duration=duration).set_start(start_time).set_position(position)
+    
+    return gif
+
 
 def overlay_fallacies_on_video(video_path, fallacy_results_file, final_video_name):
     try:
@@ -285,18 +358,16 @@ def overlay_fallacies_on_video(video_path, fallacy_results_file, final_video_nam
         original_width, original_height = video.size
         video_duration = video.duration
 
-        # Calculate sizes based on video dimensions
-        sidebar_width = int(original_width * 0.25)  # 25% of original width
-        bottom_height = int(original_height * 0.2)  # 20% of original height
+        # Sizes based on video dimensions
+        sidebar_width = int(original_width * 0.25)
+        bottom_height = int(original_height * 0.15)
         new_width = original_width + sidebar_width
         new_height = original_height + bottom_height
 
-        # Calculate font sizes based on video height
-        main_font_size = max(int(original_height * 0.04), 12)  # 2% of height, minimum 12
-        subtitle_font_size = max(int(original_height * 0.033), 10)  # 1.5% of height, minimum 10
-        
-        # Define the position for the explanation box (e.g., 30% down from the top of the video)
-        explanation_top_position = int(original_height * 0.25)  
+        # Font sizes based on video height
+        main_font_size = max(int(original_height * 0.04), 12)
+        subtitle_font_size = max(int(original_height * 0.033), 10)
+        explanation_top_position = int(original_height * 0.25)
 
         background = ColorClip(size=(new_width, new_height), color=(0, 0, 0)).set_duration(video_duration)
         video = video.set_position((0, 0))
@@ -304,38 +375,49 @@ def overlay_fallacies_on_video(video_path, fallacy_results_file, final_video_nam
         with open(fallacy_results_file, 'r') as json_file:
             fallacy_results = json.load(json_file)
 
+        # Assignment of colors based on the number of unique speakers
+        speakers = [result["speaker"] for result in fallacy_results if 'SPEAKER' in result["speaker"]]
+        unique_speakers = list(set(speakers))
+        speaker_colors = dict(zip(unique_speakers, generate_pastel_colors(len(unique_speakers))))
+        
+        
         fallacy_counters = {}
-        speaker_colors = {}
+        start_end_times = []  # Store start and end times
         for result in fallacy_results:
             speaker = result["speaker"]
-            if speaker not in fallacy_counters:
+            if speaker not in fallacy_counters and 'SPEAKER' in speaker:
                 fallacy_counters[speaker] = 0
-                speaker_colors[speaker] = generate_pastel_color()
+
+            start_time = max(0, min(result["start"], video_duration))
+            end_time = max(start_time, min(result["end"], video_duration))
+            start_end_times.append((start_time, end_time))
 
         def create_scoreboard(counters):
             scoreboard_text = "Fallacy Scoreboard\n" + "\n".join([f"{s}: {c}" for s, c in counters.items()])
-            return TextClip(scoreboard_text, fontsize=main_font_size, color='white', bg_color='black', size=(sidebar_width, None), method='caption', align='center')
+            return TextClip(scoreboard_text, fontsize=main_font_size, color='white', bg_color='black', size=(sidebar_width, None), method='caption', align='West', stroke_color='white', stroke_width=1.0)
 
         scoreboard = create_scoreboard(fallacy_counters)
         scoreboard = scoreboard.set_position((original_width, 0)).set_duration(video_duration)
 
         text_clips = [scoreboard]
+        gif_clips = []
         current_fallacy = None
 
-        for result in fallacy_results:
+        for i, result in enumerate(fallacy_results):
             start_time = max(0, min(result["start"], video_duration))
             end_time = max(start_time, min(result["end"], video_duration))
             speaker = result["speaker"]
             fallacies = result["fallacy_type"]
             reason = result["fallacy_explanation"]
             text_segment = result["text_segment"]
+            gif_query = result["gif_query"]
 
             if not isinstance(fallacies, list):
                 fallacies = [fallacies]
 
             valid_fallacies = [f for f in fallacies if f not in [None, "None"]]
 
-            if valid_fallacies:
+            if valid_fallacies and 'SPEAKER' in speaker:
                 fallacy_counters[speaker] += len(valid_fallacies)
                 new_scoreboard = create_scoreboard(fallacy_counters)
                 new_scoreboard = new_scoreboard.set_position((original_width, 0)).set_start(start_time)
@@ -343,15 +425,9 @@ def overlay_fallacies_on_video(video_path, fallacy_results_file, final_video_nam
 
                 fallacy_text = f"Speaker: {speaker}\nFallacies: {', '.join(valid_fallacies)}\n\nExplanation:\n{reason}"
                 
-                # Create the fallacy box without specifying a fixed height
-                fallacy_box = TextClip(fallacy_text, fontsize=subtitle_font_size, color=speaker_colors[speaker], 
-                                    bg_color='black', method='caption', 
-                                    size=(sidebar_width - 20, None),
-                                    stroke_color='white', stroke_width=1)
+                fallacy_box = TextClip(fallacy_text, fontsize=subtitle_font_size, color=speaker_colors[speaker], bg_color='black', method='caption', align='West', size=(sidebar_width - 20, None))
                 
                 fallacy_composite = CompositeVideoClip([fallacy_box.set_opacity(0.8)], size=fallacy_box.size)
-                
-                # Set the position of the fallacy box lower on the screen
                 fallacy_composite = fallacy_composite.set_position((original_width + 10, explanation_top_position)).set_start(start_time).set_end(end_time)
                 
                 text_clips.append(fallacy_composite)
@@ -359,24 +435,37 @@ def overlay_fallacies_on_video(video_path, fallacy_results_file, final_video_nam
             elif current_fallacy:
                 extended_fallacy = current_fallacy.set_end(end_time)
                 text_clips.append(extended_fallacy)
-
+                
+            gif_dir = os.path.join(os.path.dirname(final_video_name), 'gifs')
+            if not os.path.exists(gif_dir):
+                os.makedirs(gif_dir)
+            gif_path = os.path.join(gif_dir, f"{gif_query}.gif")
+            if not os.path.exists(gif_path):
+                gif_path = search_and_download_gif(gif_query, gif_path, limit=1)
+                
+            if gif_path: # skip if just added the gif
+                gif_clip = add_gif_to_video(original_height, new_height, new_width, gif_path, start_time, end_time, sidebar_width)
+                gif_clips.append(gif_clip)
+                
+            else:
+                gif_clips.append(None)
+                
             subtitle = TextClip(text_segment, fontsize=subtitle_font_size, color=speaker_colors[speaker], bg_color='black', method='caption', size=(original_width, None))
             subtitle = subtitle.set_position((0, original_height)).set_start(start_time).set_end(end_time)
             text_clips.append(subtitle)
 
-        # If there's remaining video duration, extend the last fallacy explanation
         if current_fallacy and current_fallacy.end < video_duration:
             final_extension = current_fallacy.set_end(video_duration)
             text_clips.append(final_extension)
 
-        final_clip = CompositeVideoClip([background, video] + text_clips, size=(new_width, new_height))
+        final_clip = CompositeVideoClip([background, video] + text_clips + [gif for gif in gif_clips if gif], size=(new_width, new_height))
         final_clip = final_clip.set_duration(video_duration)
 
         final_clip.write_videofile(final_video_name, codec="libx264", audio_codec="aac", threads=16, fps=24)
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-
+        
 def fallacy_detection_pipeline(youtube_url, output_path):
     name = youtube_url.split('=')[-1]
 
@@ -432,7 +521,7 @@ def fallacy_detection_pipeline(youtube_url, output_path):
 
 if __name__ == "__main__": 
     output_path = os.path.abspath(Path("./Files"))
-    video_url = "https://www.youtube.com/watch?v=Z3eCCbVr3EU"
+    video_url = 'https://www.youtube.com/watch?v=LM-bjbeqaqE' #"https://www.youtube.com/watch?v=Z3eCCbVr3EU"
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     
